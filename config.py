60d02fa6f4093e4a5e361b28edd71f02b8a553fc
ankurjoshi00 <ankur.joshi212@gmail.com>
2025-07-10 20:07:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,11 +21,11 @@ class Config:
         """Setup environment variables."""
         try:
             from google.colab import userdata
-            os.environ["GOOGLE_API_KEY"] = userdata.get('AIzaSyBAJI2yAC8sGnZEWCN8MPadY3QjrsJwiFc')
-            os.environ["GOOGLE_CSE_ID"] = userdata.get('24086f09671284832')
+            os.environ["GOOGLE_API_KEY"] = userdata.get('GOOGLE_API_KEY')
+            os.environ["GOOGLE_CSE_ID"] = userdata.get('GOOGLE_CSE_ID')
         except ImportError:
-            os.environ["GOOGLE_API_KEY"] = "AIzaSyBAJI2yAC8sGnZEWCN8MPadY3QjrsJwiFc"
-            os.environ["GOOGLE_CSE_ID"] = "24086f09671284832"
+            os.environ["GOOGLE_API_KEY"] = "GOOGLE_API_KEY"
+            os.environ["GOOGLE_CSE_ID"] = "GOOGLE_CSE_ID"
     
     def _validate_config(self):
         """Validate configuration."""
